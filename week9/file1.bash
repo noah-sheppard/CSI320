4de52d1d006a0cfe3ca934b7d5c77659f3dd5be3
noah-sheppard <160634648+noah-sheppard@users.noreply.github.com>
2024-10-24 14:33:30 -0400

    Update file1.bash

--- week9/file1.bash
+++ week9/file1.bash
@@ -1,1 +1,11 @@
+[ $# -ne 1 ] && { echo "Usage: $0 <Prefix>"; exit 1; }
+
+prefix=$1
+
+[ ${#prefix} -lt 5 ] && { printf "Prefix length is too short\n Prefix example: 10.0.17\n"; exit 1; }
+
+for i in $(seq 1 254); do
+         if ping -c 1 -W 1 "$prefix.$i" &>/dev/null; then echo $prefix.$i
+         fi
+done
 
