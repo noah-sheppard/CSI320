8d82402217cc804d22ad890595c01840c2157e5a
noah-sheppard <160634648+noah-sheppard@users.noreply.github.com>
2024-12-10 01:48:03 -0500

    Update scrapeIOC.bash

--- FinalExam/scrapeIOC.bash
+++ FinalExam/scrapeIOC.bash
@@ -8,4 +8,4 @@ curl -s "$IOC_URL" | \
   grep -oP '(?<=<td>)[^<]+(?=</td>)' | \
   awk 'NR % 2 == 1' > "$OUTPUT_FILE" 
 
-echo "Indicators of Compromise saved to $OUTPUT_FILE"
+echo "Saved to $OUTPUT_FILE"
